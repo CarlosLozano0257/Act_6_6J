ce668653c8a4457a5ca4b86d996951a17ca195c7
a.23308051280257 <a.23308051280257@cbtis128.edu.mx>
2026-02-11 18:28:04 +0000

    gameshop

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const AppGameshop());
+
+class AppGameshop extends StatelessWidget {
+  const AppGameshop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Gameshop",
+      home: Catalogo(),
+    );
+  }
+} //fin de clase AppGameshop
+
+class Catalogo extends StatelessWidget {
+  const Catalogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalogo de Juegos"),
+        centerTitle: true,
+        backgroundColor: Colors.cyanAccent,
+        leading: Icon(Icons.flight),
+        actions: const [
+          Icon(Icons.more_vert),
+          Icon(Icons.favorite),
+        ],
+      ),
+    );
+  }
+}
